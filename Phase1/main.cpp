e8253245a8984242dc19baa7ea04fe86407616ab
arishaabrar8-sudo <arishaabrar8@gmail.com>
2026-07-23 10:52:45 +0500

    Implement folder scanning in main function
    main.cpp in c++

--- Phase1/main.cpp
+++ Phase1/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <filesystem>
+#include "config.h"
+#include "Scanner.h"
 
+namespace fs = std::filesystem;
+
+int main()
+{
+    std::cout << "====== DoomFolder ======\n\n";
+
+    std::string folder;
+
+    std::cout << "Enter folder path: ";
+    std::getline(std::cin, folder);
+
+    Scanner scanner;
+    scanner.scanFolder(folder);
+
+    return 0;
+}
