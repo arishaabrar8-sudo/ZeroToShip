aaf060cd9c9c1df1a704e92d3982c2d59e43fd94
arishaabrar8-sudo <arishaabrar8@gmail.com>
2026-08-09 12:49:46 +0500

    Create FileData.cpp

---
+++ phase5/FileData.cpp
@@ -0,0 +1,16 @@
+#include "FileData.h"
+#include <string>
+
+FileData::FileData(
+    const std::string& n,
+    const std::string& ext,
+    const std::string& p,
+    uintmax_t s,
+    const std::string& lm)
+{
+    name = n;
+    extension = ext;
+    path = p;
+    size = s;
+    lastModified = lm;
+}
